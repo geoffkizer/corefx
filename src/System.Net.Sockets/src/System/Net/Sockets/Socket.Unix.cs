b7357e5e14c9b892e17e5dff6eb0641df74838a8
Geoff Kizer <geoffrek@microsoft.com>
2017-01-05 18:30:32 -0800

    fix SendFile

--- src/System.Net.Sockets/src/System/Net/Sockets/Socket.Unix.cs
+++ src/System.Net.Sockets/src/System/Net/Sockets/Socket.Unix.cs
@@ -92,10 +92,13 @@ namespace System.Net.Sockets
                 {
                     var tcs = new TaskCompletionSource<bool>();
 
+                    // CONSIDER: this is a little scary, having to check the sync success thing...
+                    // Is there a better way to do this?
+
                     // This can throw ObjectDisposedException.
                     errorCode = SocketPal.SendFileAsync(_handle, fileStream, (bytesTransferred, socketError) => 
                     {
-                        if (socketError != SocketError.Success)
+                        if (socketError != SocketError.Success && socketError != SocketErrorExt.SynchronousSuccess)
                         {
                             // Synchronous exception from SendFileAsync
                             SocketException socketException = new SocketException((int)errorCode);
