417446d0efa4ecf41a2f38cd825182aa98fba82b
Geoff Kizer <geoffrek@microsoft.com>
2017-01-31 22:30:22 -0800

    misc

--- src/System.Net.Sockets/tests/PerformanceTests/SocketPerformanceAsyncTests.cs
+++ src/System.Net.Sockets/tests/PerformanceTests/SocketPerformanceAsyncTests.cs
@@ -62,7 +62,7 @@ namespace System.Net.Sockets.Performance.Tests
             int iterations = 200 * _iterations;
             int bufferSize = 256;
 //            int socket_instances = 500;
-            int socket_instances = 10;
+            int socket_instances = 100;
 
             var test = new SocketPerformanceTests(_log);
 
