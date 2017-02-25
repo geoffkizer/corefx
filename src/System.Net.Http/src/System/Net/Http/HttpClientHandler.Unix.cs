171e9acdfed2408b058f4b1ce03fbcfbb6b4b47e
Geoff Kizer <geoffrek@microsoft.com>
2017-02-24 17:46:03 -0800

    enable on Linux too

--- src/System.Net.Http/src/System/Net/Http/HttpClientHandler.Unix.cs
+++ src/System.Net.Http/src/System/Net/Http/HttpClientHandler.Unix.cs
@@ -1,3 +1,4 @@
+#if false
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
@@ -178,3 +179,4 @@ namespace System.Net.Http
         #endregion Private
     }
 }
+#endif
