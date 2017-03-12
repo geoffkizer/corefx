9279a110c6b88dfa7567a56e506cc761439ece15
Geoff Kizer <geoffrek@microsoft.com>
2017-03-12 12:25:09 -0700

    add and use TryGetKnownHeader

--- src/System.Net.Http/src/System/Net/Http/Headers/HttpKnownHeaderInfos.cs
+++ src/System.Net.Http/src/System/Net/Http/Headers/HttpKnownHeaderInfos.cs
@@ -26,15 +26,15 @@ namespace System.Net.Http.Headers
             return !left.Equals(right);
         }
 
-        // TODO: Something like static "Get"
         public static HeaderInfo Get(string name)
         {
             Debug.Assert(!string.IsNullOrEmpty(name));
             Debug.Assert(HttpRuleParser.GetTokenLength(name, 0) == name.Length);
 
-            // TODO
-            return null;
+            // This is stupid and is only for testing purposes
+            var charArray = name.ToCharArray();
 
+            return KnownHeaders.TryGetKnownHeader(charArray, 0, charArray.Length) ?? new CustomHeaderInfo(name);
         }
 
         private sealed class CustomHeaderInfo : HeaderInfo
@@ -82,7 +82,7 @@ namespace System.Net.Http.Headers
             public override bool Equals(HeaderInfo other)
             {
                 // Reference equality
-                return this == other;
+                return (object)this == (object)other;
             }
 
             public override int GetHashCode()
@@ -93,7 +93,7 @@ namespace System.Net.Http.Headers
 
         internal static class KnownHeaders
         {
-            // If you add a new entry here, you need to add it to TryGetKnownHeader above as well.
+            // If you add a new entry here, you need to add it to TryGetKnownHeader below as well.
 
             public static HeaderInfo Accept = new KnownHeaderInfo("Accept");
             public static HeaderInfo AcceptCharset = new KnownHeaderInfo("Accept-Charset");
@@ -178,6 +178,267 @@ namespace System.Net.Http.Headers
             public static HeaderInfo XPoweredBy = new KnownHeaderInfo("X-Powered-By");
             public static HeaderInfo XRequestID = new KnownHeaderInfo("X-Request-ID");
             public static HeaderInfo XUACompatible = new KnownHeaderInfo("X-UA-Compatible");
+
+            // TODO: This isn't quite right, as it only matches known headers with the standard capitalization (as listed above).
+            // We need to handle cases where non-standard capitalization is used, e.g. "content-length".
+            // These need to be treated as known headers, but have their capitalization preserved.
+            // This probably means something like adding a "NonstandardKnownHeaderInfo".  Ugh.
+
+            internal static HeaderInfo TryGetKnownHeader(char[] key, int startIndex, int length)
+            {
+                Debug.Assert(key != null);
+                Debug.Assert(startIndex >= 0);
+                Debug.Assert(length >= 0);
+
+                // When adding a new constant, add it to HttpKnownHeaderNames.cs as well.
+
+                // The lookup works as follows: first switch on the length of the passed-in key.
+                //
+                //  - If there is only one known header of that length, set potentialHeader to that known header
+                //    and goto TryMatch to see if the key fully matches potentialHeader.
+                //
+                //  - If there are more than one known headers of that length, switch on a unique char from that
+                //    set of same-length known headers. Typically this will be the first char, but some sets of
+                //    same-length known headers do not have unique chars in the first position, so a char in a
+                //    position further in the strings is used. If the char from the key matches one of the
+                //    known headers, set potentialHeader to that known header and goto TryMatch to see if the key
+                //    fully matches potentialHeader.
+                //
+                //  - Otherwise, there is no match, so set the out param to null and return false.
+                //
+                // Matching is case-sensitive: we only want to return a known header that exactly matches the key.
+
+                HeaderInfo potentialHeader = null;
+
+                switch (length)
+                {
+                    case 2:
+                        potentialHeader = TE; goto TryMatch; // TE
+
+                    case 3:
+                        switch (key[startIndex])
+                        {
+                            case 'A': potentialHeader = Age; goto TryMatch; // [A]ge
+                            case 'P': potentialHeader = P3P; goto TryMatch; // [P]3P
+                            case 'T': potentialHeader = TSV; goto TryMatch; // [T]SV
+                            case 'V': potentialHeader = Via; goto TryMatch; // [V]ia
+                        }
+                        break;
+
+                    case 4:
+                        switch (key[startIndex])
+                        {
+                            case 'D': potentialHeader = Date; goto TryMatch; // [D]ate
+                            case 'E': potentialHeader = ETag; goto TryMatch; // [E]Tag
+                            case 'F': potentialHeader = From; goto TryMatch; // [F]rom
+                            case 'H': potentialHeader = Host; goto TryMatch; // [H]ost
+                            case 'L': potentialHeader = Link; goto TryMatch; // [L]ink
+                            case 'V': potentialHeader = Vary; goto TryMatch; // [V]ary
+                        }
+                        break;
+
+                    case 5:
+                        switch (key[startIndex])
+                        {
+                            case 'A': potentialHeader = Allow; goto TryMatch; // [A]llow
+                            case 'R': potentialHeader = Range; goto TryMatch; // [R]ange
+                        }
+                        break;
+
+                    case 6:
+                        switch (key[startIndex])
+                        {
+                            case 'A': potentialHeader = Accept; goto TryMatch; // [A]ccept
+                            case 'C': potentialHeader = Cookie; goto TryMatch; // [C]ookie
+                            case 'E': potentialHeader = Expect; goto TryMatch; // [E]xpect
+                            case 'O': potentialHeader = Origin; goto TryMatch; // [O]rigin
+                            case 'P': potentialHeader = Pragma; goto TryMatch; // [P]ragma
+                            case 'S': potentialHeader = Server; goto TryMatch; // [S]erver
+                        }
+                        break;
+
+                    case 7:
+                        switch (key[startIndex])
+                        {
+                            case 'A': potentialHeader = AltSvc; goto TryMatch;  // [A]lt-Svc
+                            case 'C': potentialHeader = Cookie2; goto TryMatch; // [C]ookie2
+                            case 'E': potentialHeader = Expires; goto TryMatch; // [E]xpires
+                            case 'R': potentialHeader = Referer; goto TryMatch; // [R]eferer
+                            case 'T': potentialHeader = Trailer; goto TryMatch; // [T]railer
+                            case 'U': potentialHeader = Upgrade; goto TryMatch; // [U]pgrade
+                            case 'W': potentialHeader = Warning; goto TryMatch; // [W]arning
+                        }
+                        break;
+
+                    case 8:
+                        switch (key[startIndex + 3])
+                        {
+                            case 'M': potentialHeader = IfMatch; goto TryMatch;  // If-[M]atch
+                            case 'R': potentialHeader = IfRange; goto TryMatch;  // If-[R]ange
+                            case 'a': potentialHeader = Location; goto TryMatch; // Loc[a]tion
+                        }
+                        break;
+
+                    case 10:
+                        switch (key[startIndex])
+                        {
+                            case 'C': potentialHeader = Connection; goto TryMatch; // [C]onnection
+                            case 'K': potentialHeader = KeepAlive; goto TryMatch;  // [K]eep-Alive
+                            case 'S': potentialHeader = SetCookie; goto TryMatch;  // [S]et-Cookie
+                            case 'U': potentialHeader = UserAgent; goto TryMatch;  // [U]ser-Agent
+                        }
+                        break;
+
+                    case 11:
+                        switch (key[startIndex])
+                        {
+                            case 'C': potentialHeader = ContentMD5; goto TryMatch; // [C]ontent-MD5
+                            case 'R': potentialHeader = RetryAfter; goto TryMatch; // [R]etry-After
+                            case 'S': potentialHeader = SetCookie2; goto TryMatch; // [S]et-Cookie2
+                        }
+                        break;
+
+                    case 12:
+                        switch (key[startIndex + 2])
+                        {
+                            case 'c': potentialHeader = AcceptPatch; goto TryMatch; // Ac[c]ept-Patch
+                            case 'n': potentialHeader = ContentType; goto TryMatch; // Co[n]tent-Type
+                            case 'x': potentialHeader = MaxForwards; goto TryMatch; // Ma[x]-Forwards
+                            case 'M': potentialHeader = XMSEdgeRef; goto TryMatch;  // X-[M]SEdge-Ref
+                            case 'P': potentialHeader = XPoweredBy; goto TryMatch;  // X-[P]owered-By
+                            case 'R': potentialHeader = XRequestID; goto TryMatch;  // X-[R]equest-ID
+                        }
+                        break;
+
+                    case 13:
+                        switch (key[startIndex + 6])
+                        {
+                            case '-': potentialHeader = AcceptRanges; goto TryMatch;  // Accept[-]Ranges
+                            case 'i': potentialHeader = Authorization; goto TryMatch; // Author[i]zation
+                            case 'C': potentialHeader = CacheControl; goto TryMatch;  // Cache-[C]ontrol
+                            case 't': potentialHeader = ContentRange; goto TryMatch;  // Conten[t]-Range
+                            case 'e': potentialHeader = IfNoneMatch; goto TryMatch;   // If-Non[e]-Match
+                            case 'o': potentialHeader = LastModified; goto TryMatch;  // Last-M[o]dified
+                        }
+                        break;
+
+                    case 14:
+                        switch (key[startIndex])
+                        {
+                            case 'A': potentialHeader = AcceptCharset; goto TryMatch; // [A]ccept-Charset
+                            case 'C': potentialHeader = ContentLength; goto TryMatch; // [C]ontent-Length
+                        }
+                        break;
+
+                    case 15:
+                        switch (key[startIndex + 7])
+                        {
+                            case '-': potentialHeader = XFrameOptions; goto TryMatch;  // X-Frame[-]Options
+                            case 'm': potentialHeader = XUACompatible; goto TryMatch;  // X-UA-Co[m]patible
+                            case 'E': potentialHeader = AcceptEncoding; goto TryMatch; // Accept-[E]ncoding
+                            case 'K': potentialHeader = PublicKeyPins; goto TryMatch;  // Public-[K]ey-Pins
+                            case 'L': potentialHeader = AcceptLanguage; goto TryMatch; // Accept-[L]anguage
+                        }
+                        break;
+
+                    case 16:
+                        switch (key[startIndex + 11])
+                        {
+                            case 'o': potentialHeader = ContentEncoding; goto TryMatch; // Content-Enc[o]ding
+                            case 'g': potentialHeader = ContentLanguage; goto TryMatch; // Content-Lan[g]uage
+                            case 'a': potentialHeader = ContentLocation; goto TryMatch; // Content-Loc[a]tion
+                            case 'c': potentialHeader = ProxyConnection; goto TryMatch; // Proxy-Conne[c]tion
+                            case 'i': potentialHeader = WWWAuthenticate; goto TryMatch; // WWW-Authent[i]cate
+                            case 'r': potentialHeader = XAspNetVersion; goto TryMatch;  // X-AspNet-Ve[r]sion
+                        }
+                        break;
+
+                    case 17:
+                        switch (key[startIndex])
+                        {
+                            case 'I': potentialHeader = IfModifiedSince; goto TryMatch;  // [I]f-Modified-Since
+                            case 'S': potentialHeader = SecWebSocketKey; goto TryMatch;  // [S]ec-WebSocket-Key
+                            case 'T': potentialHeader = TransferEncoding; goto TryMatch; // [T]ransfer-Encoding
+                        }
+                        break;
+
+                    case 18:
+                        switch (key[startIndex])
+                        {
+                            case 'P': potentialHeader = ProxyAuthenticate; goto TryMatch; // [P]roxy-Authenticate
+                            case 'X': potentialHeader = XContentDuration; goto TryMatch;  // [X]-Content-Duration
+                        }
+                        break;
+
+                    case 19:
+                        switch (key[startIndex])
+                        {
+                            case 'C': potentialHeader = ContentDisposition; goto TryMatch; // [C]ontent-Disposition
+                            case 'I': potentialHeader = IfUnmodifiedSince; goto TryMatch;  // [I]f-Unmodified-Since
+                            case 'P': potentialHeader = ProxyAuthorization; goto TryMatch; // [P]roxy-Authorization
+                        }
+                        break;
+
+                    case 20:
+                        potentialHeader = SecWebSocketAccept; goto TryMatch; // Sec-WebSocket-Accept
+
+                    case 21:
+                        potentialHeader = SecWebSocketVersion; goto TryMatch; // Sec-WebSocket-Version
+
+                    case 22:
+                        switch (key[startIndex])
+                        {
+                            case 'A': potentialHeader = AccessControlMaxAge; goto TryMatch;  // [A]ccess-Control-Max-Age
+                            case 'S': potentialHeader = SecWebSocketProtocol; goto TryMatch; // [S]ec-WebSocket-Protocol
+                            case 'X': potentialHeader = XContentTypeOptions; goto TryMatch;  // [X]-Content-Type-Options
+                        }
+                        break;
+
+                    case 23:
+                        potentialHeader = ContentSecurityPolicy; goto TryMatch; // Content-Security-Policy
+
+                    case 24:
+                        potentialHeader = SecWebSocketExtensions; goto TryMatch; // Sec-WebSocket-Extensions
+
+                    case 25:
+                        switch (key[startIndex])
+                        {
+                            case 'S': potentialHeader = StrictTransportSecurity; goto TryMatch; // [S]trict-Transport-Security
+                            case 'U': potentialHeader = UpgradeInsecureRequests; goto TryMatch; // [U]pgrade-Insecure-Requests
+                        }
+                        break;
+
+                    case 27:
+                        potentialHeader = AccessControlAllowOrigin; goto TryMatch; // Access-Control-Allow-Origin
+
+                    case 28:
+                        switch (key[startIndex + 21])
+                        {
+                            case 'H': potentialHeader = AccessControlAllowHeaders; goto TryMatch; // Access-Control-Allow-[H]eaders
+                            case 'M': potentialHeader = AccessControlAllowMethods; goto TryMatch; // Access-Control-Allow-[M]ethods
+                        }
+                        break;
+
+                    case 29:
+                        potentialHeader = AccessControlExposeHeaders; goto TryMatch; // Access-Control-Expose-Headers
+
+                    case 32:
+                        potentialHeader = AccessControlAllowCredentials; goto TryMatch; // Access-Control-Allow-Credentials
+                }
+
+                return null;
+
+                TryMatch:
+                Debug.Assert(potentialHeader != null);
+                Debug.Assert(potentialHeader.Name.Length == length);
+
+                if (CharArrayHelpers.EqualsOrdinal(potentialHeader.Name, key, startIndex, length))
+                {
+                    return potentialHeader;
+                }
+
+                return null;
+            }
         }
     }
 }
