b3f1cb58d51674dc8f1d58d30ebc9006e6e01201
Geoff Kizer <geoffrek>
2017-07-24 10:07:29 -0700

    add test for setting nonblocking and timeout while pending sync receive

---
+++ src/System.Net.Sockets/tests/FunctionalTests/SetNonBlockingTest.cs
@@ -0,0 +1,115 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace System.Net.Sockets.Tests
+{
+    public class SetNonBlockingTest
+    {
+        [Fact]
+        public async void SetNonBlocking()
+        {
+            using (var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                server.BindToAnonymousPort(IPAddress.Loopback);
+                server.Listen(1);
+
+                Task serverTask = server.AcceptAsync();
+
+                EndPoint clientEndpoint = server.LocalEndPoint;
+                using (var client = new Socket(clientEndpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    await client.ConnectAsync(clientEndpoint);
+                    await serverTask;
+
+                    DateTime start = DateTime.UtcNow;
+
+                    // Hang a blocking receive
+                    Task receiveTask = Task.Run(() =>
+                    {
+                        Console.WriteLine("About to sync Receive");
+
+                        try
+                        {
+                            client.Receive(new byte[1]);
+                        }
+
+                        catch (Exception e)
+                        {
+                            Console.Write($"Caught exception, time={DateTime.UtcNow - start}, e={e}");
+                        }
+
+                        Console.WriteLine("Receive completed");
+                    });
+
+                    // Wait a bit and then change to nonblocking
+                    Thread.Sleep(5000);
+
+                    Console.WriteLine("About to set nonblocking");
+
+                    client.Blocking = false;
+
+                    bool completed = receiveTask.Wait(5000);
+                    Console.WriteLine($"Wait complete, time={DateTime.UtcNow - start}, completed={completed}");
+                }
+            }
+        }
+
+        [Fact]
+        public async void SetTimeout()
+        {
+            using (var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                server.BindToAnonymousPort(IPAddress.Loopback);
+                server.Listen(1);
+
+                Task serverTask = server.AcceptAsync();
+
+                EndPoint clientEndpoint = server.LocalEndPoint;
+                using (var client = new Socket(clientEndpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    await client.ConnectAsync(clientEndpoint);
+                    await serverTask;
+
+                    DateTime start = DateTime.UtcNow;
+
+                    // Hang a blocking receive
+                    Task receiveTask = Task.Run(() =>
+                    {
+                        Console.WriteLine("About to sync Receive");
+
+                        try
+                        {
+                            client.Receive(new byte[1]);
+                        }
+
+                        catch (Exception e)
+                        {
+                            Console.Write($"Caught exception, time={DateTime.UtcNow - start}, e={e}");
+                        }
+
+                        Console.WriteLine("Receive completed");
+                    });
+
+                    // Wait a bit and then change to nonblocking
+                    Thread.Sleep(5000);
+
+                    Console.WriteLine("About to set timeout");
+
+                    client.ReceiveTimeout = 1000;
+
+                    bool completed = receiveTask.Wait(5000);
+                    Console.WriteLine($"Wait complete, time={DateTime.UtcNow - start}, completed={completed}");
+                }
+            }
+        }
+    }
+}
