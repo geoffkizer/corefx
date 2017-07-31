a5d62fb52b030d0af3ec967b42318aa1bf609b55
Geoff Kizer <geoffrek>
2017-07-31 12:01:02 -0700

    add ShutdownEffects test class

--- src/System.Net.Sockets/tests/FunctionalTests/SetNonBlockingTest.cs
+++ src/System.Net.Sockets/tests/FunctionalTests/SetNonBlockingTest.cs
@@ -329,4 +329,185 @@ namespace System.Net.Sockets.Tests
             }
         }
     }
+
+    public abstract class ShutdownEffects<T> : SocketTestHelperBase<T> where T : SocketHelperBase, new()
+    {
+        // I really want something like the following tests:
+        // (1) Shutdown with pending (recv, recv async, send, send async) + force nonblocking
+        // (2) Close with same
+
+        [Theory]
+        [InlineData(true, false)]
+        [InlineData(false, true)]
+        [InlineData(true, true)]
+        [InlineData(false, false)]
+        public async void TestClose(bool doReceive, bool doSend)
+        {
+            using (var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                server.BindToAnonymousPort(IPAddress.Loopback);
+                server.Listen(1);
+
+                Task serverTask = AcceptAsync(server);
+
+                EndPoint clientEndpoint = server.LocalEndPoint;
+                using (var client = new Socket(clientEndpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    await ConnectAsync(client, clientEndpoint);
+                    await serverTask;
+
+                    // Hang operations
+                    Task<int> receiveTask = null;
+                    if (doReceive)
+                    {
+                        receiveTask = ReceiveAsync(client, new ArraySegment<byte>(new byte[1]));
+                    }
+
+                    Task sendTask = null;
+                    if (doSend)
+                    {
+                        sendTask = Task.Run(async () =>
+                        {
+                            byte[] sendBuffer = new byte[16 * 1024];
+                            while (true)
+                            {
+                                await SendAsync(client, new ArraySegment<byte>(sendBuffer));
+                            }
+                        });
+                    }
+
+                    // Wait a bit to hopefully allow the Send code to max out the send buffer and block
+                    Thread.Sleep(1000);
+
+                    Console.WriteLine("TestClose: About to Close");
+                    client.Close();
+                    Console.WriteLine("TestClose: Closed");
+
+                    if (doReceive)
+                    {
+                        try
+                        {
+                            int result = await receiveTask;
+                            Console.WriteLine($"TestShutdownEffects Receive: bytes={result}");
+                        }
+                        catch (SocketException e)
+                        {
+                            Console.WriteLine($"TestShutdownEffects Receive: errorCode={e.SocketErrorCode}");
+                        }
+                    }
+
+                    if (doSend)
+                    {
+                        try
+                        {
+                            await sendTask;
+                            Console.WriteLine($"TestShutdownEffects Send completed");
+                        }
+                        catch (SocketException e)
+                        {
+                            Console.WriteLine($"TestShutdownEffects Send: errorCode={e.SocketErrorCode}");
+                        }
+                    }
+                }
+            }
+        }
+
+        [Theory]
+        [InlineData(true, false)]
+        [InlineData(false, true)]
+        [InlineData(true, true)]
+        [InlineData(false, false)]
+        public async void TestShutdown(bool doReceive, bool doSend)
+        {
+            using (var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                server.BindToAnonymousPort(IPAddress.Loopback);
+                server.Listen(1);
+
+                Task serverTask = AcceptAsync(server);
+
+                EndPoint clientEndpoint = server.LocalEndPoint;
+                using (var client = new Socket(clientEndpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    await ConnectAsync(client, clientEndpoint);
+                    await serverTask;
+
+                    // Hang operations
+                    Task<int> receiveTask = null;
+                    if (doReceive)
+                    {
+                        receiveTask = ReceiveAsync(client, new ArraySegment<byte>(new byte[1]));
+                    }
+
+                    Task sendTask = null;
+                    if (doSend)
+                    {
+                        sendTask = Task.Run(async () =>
+                        {
+                            byte[] sendBuffer = new byte[16 * 1024];
+                            while (true)
+                            {
+                                await SendAsync(client, new ArraySegment<byte>(sendBuffer));
+                            }
+                        });
+                    }
+
+                    // Wait a bit to hopefully allow the Send code to max out the send buffer and block
+                    Thread.Sleep(1000);
+
+                    Console.WriteLine("TestShutdown: About to Shutdown");
+                    client.Shutdown(SocketShutdown.Both);
+                    Console.WriteLine("TestShutdown: Shutdown");
+
+                    if (doReceive)
+                    {
+                        try
+                        {
+                            bool completed = receiveTask.Wait(1000);
+                            if (completed)
+                            {
+                                int result = await receiveTask;
+                                Console.WriteLine($"TestShutdownEffects Receive: bytes={result}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"TestShutdownEffects Receive timed out");
+                            }
+                        }
+                        catch (SocketException e)
+                        {
+                            Console.WriteLine($"TestShutdownEffects Receive: errorCode={e.SocketErrorCode}");
+                        }
+                    }
+
+                    if (doSend)
+                    {
+                        try
+                        {
+                            bool completed = sendTask.Wait(1000);
+                            if (completed)
+                            {
+                                await sendTask;
+                                Console.WriteLine($"TestShutdownEffects Send completed");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"TestShutdownEffects Send timed out");
+                            }
+                        }
+                        catch (SocketException e)
+                        {
+                            Console.WriteLine($"TestShutdownEffects Send: errorCode={e.SocketErrorCode}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public sealed class ShutdownEffectsSync : ShutdownEffects<SocketHelperSync> { }
+    public sealed class ShutdownEffectsSyncForceNonBlocking : ShutdownEffects<SocketHelperSyncForceNonBlocking> { }
+    public sealed class ShutdownEffectsApm : ShutdownEffects<SocketHelperApm> { }
+    public sealed class ShutdownEffectsTask : ShutdownEffects<SocketHelperTask> { }
+    public sealed class ShutdownEffectsEap : ShutdownEffects<SocketHelperEap> { }
 }
