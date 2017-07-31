ce5003b6c100631779fa5d2f0e781c1561c9f324
Geoff Kizer <geoffrek>
2017-07-31 13:58:25 -0700

    reorganization and "After" tests

--- src/System.Net.Sockets/tests/FunctionalTests/SetNonBlockingTest.cs
+++ src/System.Net.Sockets/tests/FunctionalTests/SetNonBlockingTest.cs
@@ -336,12 +336,76 @@ namespace System.Net.Sockets.Tests
         // (1) Shutdown with pending (recv, recv async, send, send async) + force nonblocking
         // (2) Close with same
 
-        [Theory]
-        [InlineData(true, false)]
-        [InlineData(false, true)]
-        [InlineData(true, true)]
-        [InlineData(false, false)]
-        public async void TestClose(bool doReceive, bool doSend)
+        private Task<int> DoPendingReceive(Socket s)
+        {
+            Task<int> receiveTask = ReceiveAsync(s, new ArraySegment<byte>(new byte[1]));
+
+            // Give the task some time to (hopefully) pend the receive
+            Thread.Sleep(1000);
+
+            return receiveTask;
+        }
+
+        private Task DoPendingSend(Socket s)
+        {
+            Task sendTask = Task.Run(async () =>
+            {
+                byte[] sendBuffer = new byte[16 * 1024];
+                while (true)
+                {
+                    await SendAsync(s, new ArraySegment<byte>(sendBuffer));
+                }
+            });
+
+            // Give the task some time to (hopefully) fill the send buffer and pend
+            Thread.Sleep(1000);
+
+            return sendTask;
+        }
+
+        // Helpers that prob go away
+        private async Task ShowResult(string s, Task<int> t)
+        {
+            try
+            {
+                if (await Task.WhenAny(t, Task.Delay(1000)) == t)
+                { 
+                    int result = await t;
+                    Console.WriteLine($"{s}: bytes={result}");
+                }
+                else
+                {
+                    Console.WriteLine($"{s}: timed out");
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"{s}: errorCode={e.SocketErrorCode}");
+            }
+        }
+
+        private async Task ShowResult(string s, Task t)
+        {
+            try
+            {
+                if (await Task.WhenAny(t, Task.Delay(1000)) == t)
+                {
+                    await t;
+                    Console.WriteLine($"{s}: Completed");
+                }
+                else
+                {
+                    Console.WriteLine($"{s}: timed out");
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"{s}: errorCode={e.SocketErrorCode}");
+            }
+        }
+
+        [Fact]
+        public async Task Close_WithPendingReceive()
         {
             using (var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
             {
@@ -350,80 +414,30 @@ namespace System.Net.Sockets.Tests
 
                 Task serverTask = AcceptAsync(server);
 
-                EndPoint clientEndpoint = server.LocalEndPoint;
-                using (var client = new Socket(clientEndpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+                using (var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
                 {
-                    await ConnectAsync(client, clientEndpoint);
+                    await ConnectAsync(client, server.LocalEndPoint);
                     await serverTask;
 
-                    // Hang operations
-                    Task<int> receiveTask = null;
-                    if (doReceive)
-                    {
-                        receiveTask = ReceiveAsync(client, new ArraySegment<byte>(new byte[1]));
-                    }
+                    Task<int> t = DoPendingReceive(client);
 
-                    Task sendTask = null;
-                    if (doSend)
-                    {
-                        sendTask = Task.Run(async () =>
-                        {
-                            byte[] sendBuffer = new byte[16 * 1024];
-                            while (true)
-                            {
-                                await SendAsync(client, new ArraySegment<byte>(sendBuffer));
-                            }
-                        });
-                    }
-
-                    // Wait a bit to hopefully allow the Send code to max out the send buffer and block
-                    Thread.Sleep(1000);
-
-                    Console.WriteLine("TestClose: About to Close");
+                    Console.WriteLine("Close_WithPendingReceive: About to Close");
 
                     bool closed = Task.Run(() => client.Close()).Wait(1000);
                     if (!closed)
                     {
-                        Console.WriteLine("TestClose: Close is hung");
+                        Console.WriteLine("Close_WithPendingReceive: Close is hung");
                         return;
                     }
-                    Console.WriteLine("TestClose: Closed");
+                    Console.WriteLine("Close_WithPendingReceive: Closed");
 
-                    if (doReceive)
-                    {
-                        try
-                        {
-                            int result = await receiveTask;
-                            Console.WriteLine($"TestShutdownEffects Receive: bytes={result}");
-                        }
-                        catch (SocketException e)
-                        {
-                            Console.WriteLine($"TestShutdownEffects Receive: errorCode={e.SocketErrorCode}");
-                        }
-                    }
-
-                    if (doSend)
-                    {
-                        try
-                        {
-                            await sendTask;
-                            Console.WriteLine($"TestShutdownEffects Send completed");
-                        }
-                        catch (SocketException e)
-                        {
-                            Console.WriteLine($"TestShutdownEffects Send: errorCode={e.SocketErrorCode}");
-                        }
-                    }
+                    await ShowResult("Close_WithPendingReceive", t);
                 }
             }
         }
 
-        [Theory]
-        [InlineData(true, false)]
-        [InlineData(false, true)]
-        [InlineData(true, true)]
-        [InlineData(false, false)]
-        public async void TestShutdown(bool doReceive, bool doSend)
+        [Fact]
+        public async Task Close_WithPendingSend()
         {
             using (var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
             {
@@ -432,80 +446,204 @@ namespace System.Net.Sockets.Tests
 
                 Task serverTask = AcceptAsync(server);
 
-                EndPoint clientEndpoint = server.LocalEndPoint;
-                using (var client = new Socket(clientEndpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+                using (var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
                 {
-                    await ConnectAsync(client, clientEndpoint);
+                    await ConnectAsync(client, server.LocalEndPoint);
                     await serverTask;
 
-                    // Hang operations
-                    Task<int> receiveTask = null;
-                    if (doReceive)
+                    Task t = DoPendingSend(client);
+
+                    Console.WriteLine("Close_WithPendingSend: About to Close");
+
+                    bool closed = Task.Run(() => client.Close()).Wait(1000);
+                    if (!closed)
                     {
-                        receiveTask = ReceiveAsync(client, new ArraySegment<byte>(new byte[1]));
+                        Console.WriteLine("Close_WithPendingSend: Close is hung");
+                        return;
                     }
+                    Console.WriteLine("Close_WithPendingSend: Closed");
 
-                    Task sendTask = null;
-                    if (doSend)
+                    await ShowResult("Close_WithPendingSend", t);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task ReceiveAfterClose()
+        {
+            using (var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                server.BindToAnonymousPort(IPAddress.Loopback);
+                server.Listen(1);
+
+                Task<Socket> serverTask = AcceptAsync(server);
+
+                using (var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    await ConnectAsync(client, server.LocalEndPoint);
+                    Socket accepted = await serverTask;
+
+                    Task<int> receiveOne = ReceiveAsync(client, new ArraySegment<byte>(new byte[1]));
+                    await SendAsync(accepted, new ArraySegment<byte>(new byte[1]));
+                    Assert.Equal(1, await receiveOne);
+
+                    Console.WriteLine("ReceiveAfterClose: About to Close");
+
+                    bool closed = Task.Run(() => client.Close()).Wait(1000);
+                    if (!closed)
                     {
-                        sendTask = Task.Run(async () =>
-                        {
-                            byte[] sendBuffer = new byte[16 * 1024];
-                            while (true)
-                            {
-                                await SendAsync(client, new ArraySegment<byte>(sendBuffer));
-                            }
-                        });
+                        Console.WriteLine("ReceiveAfterClose: Close is hung");
+                        return;
                     }
+                    Console.WriteLine("ReceiveAfterClose: Closed");
 
-                    // Wait a bit to hopefully allow the Send code to max out the send buffer and block
-                    Thread.Sleep(1000);
+                    Task<int> t = ReceiveAsync(client, new ArraySegment<byte>(new byte[1]));
+                    await ShowResult("ReceiveAfterClose", t);
+                }
+            }
+        }
 
-                    Console.WriteLine("TestShutdown: About to Shutdown");
-                    client.Shutdown(SocketShutdown.Both);
-                    Console.WriteLine("TestShutdown: Shutdown");
+        [Fact]
+        public async Task SendAfterClose()
+        {
+            using (var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                server.BindToAnonymousPort(IPAddress.Loopback);
+                server.Listen(1);
 
-                    if (doReceive)
+                Task<Socket> serverTask = AcceptAsync(server);
+
+                using (var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    await ConnectAsync(client, server.LocalEndPoint);
+                    Socket accepted = await serverTask;
+
+                    Task sendOne = SendAsync(client, new ArraySegment<byte>(new byte[1]));
+                    Assert.Equal(1, await ReceiveAsync(accepted, new ArraySegment<byte>(new byte[1])));
+                    await sendOne;
+
+                    Console.WriteLine("SendAfterClose: About to Close");
+
+                    bool closed = Task.Run(() => client.Close()).Wait(1000);
+                    if (!closed)
                     {
-                        try
-                        {
-                            bool completed = receiveTask.Wait(1000);
-                            if (completed)
-                            {
-                                int result = await receiveTask;
-                                Console.WriteLine($"TestShutdownEffects Receive: bytes={result}");
-                            }
-                            else
-                            {
-                                Console.WriteLine($"TestShutdownEffects Receive timed out");
-                            }
-                        }
-                        catch (SocketException e)
-                        {
-                            Console.WriteLine($"TestShutdownEffects Receive: errorCode={e.SocketErrorCode}");
-                        }
+                        Console.WriteLine("SendAfterClose: Close is hung");
+                        return;
                     }
+                    Console.WriteLine("SendAfterClose: Closed");
 
-                    if (doSend)
-                    {
-                        try
-                        {
-                            bool completed = sendTask.Wait(1000);
-                            if (completed)
-                            {
-                                await sendTask;
-                                Console.WriteLine($"TestShutdownEffects Send completed");
-                            }
-                            else
-                            {
-                                Console.WriteLine($"TestShutdownEffects Send timed out");
-                            }
-                        }
-                        catch (SocketException e)
-                        {
-                            Console.WriteLine($"TestShutdownEffects Send: errorCode={e.SocketErrorCode}");
-                        }
-                    }
+                    Task t = SendAsync(client, new ArraySegment<byte>(new byte[1]));
+                    await ShowResult("SendAfterClose", t);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Shutdown_WithPendingReceive()
+        {
+            using (var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                server.BindToAnonymousPort(IPAddress.Loopback);
+                server.Listen(1);
+
+                Task serverTask = AcceptAsync(server);
+
+                using (var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    await ConnectAsync(client, server.LocalEndPoint);
+                    await serverTask;
+
+                    Task<int> t = DoPendingReceive(client);
+
+                    Console.WriteLine("Shutdown_WithPendingReceive: About to Shutdown");
+                    client.Shutdown(SocketShutdown.Receive);
+                    Console.WriteLine("Shutdown_WithPendingReceive: Shutdown");
+
+                    await ShowResult("Shutdown_WithPendingReceive", t);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Shutdown_WithPendingSend()
+        {
+            using (var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                server.BindToAnonymousPort(IPAddress.Loopback);
+                server.Listen(1);
+
+                Task serverTask = AcceptAsync(server);
+
+                using (var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    await ConnectAsync(client, server.LocalEndPoint);
+                    await serverTask;
+
+                    Task t = DoPendingSend(client);
+
+                    Console.WriteLine("Shutdown_WithPendingSend: About to Shutdown");
+                    client.Shutdown(SocketShutdown.Send);
+                    Console.WriteLine("Shutdown_WithPendingSend: Shutdown");
+
+                    await ShowResult("Shutdown_WithPendingSend", t);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task ReceiveAfterShutdown()
+        {
+            using (var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                server.BindToAnonymousPort(IPAddress.Loopback);
+                server.Listen(1);
+
+                Task<Socket> serverTask = AcceptAsync(server);
+
+                using (var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    await ConnectAsync(client, server.LocalEndPoint);
+                    Socket accepted = await serverTask;
+
+                    Task<int> receiveOne = ReceiveAsync(client, new ArraySegment<byte>(new byte[1]));
+                    await SendAsync(accepted, new ArraySegment<byte>(new byte[1]));
+                    Assert.Equal(1, await receiveOne);
+
+                    Console.WriteLine("ReceiveAfterShutdown: About to Shutdown");
+                    client.Shutdown(SocketShutdown.Receive);
+                    Console.WriteLine("ReceiveAfterShutdown: Shutdown completed");
+
+                    Task<int> t = ReceiveAsync(client, new ArraySegment<byte>(new byte[1]));
+                    await ShowResult("ReceiveAfterShutdown", t);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task SendAfterShutdown()
+        {
+            using (var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                server.BindToAnonymousPort(IPAddress.Loopback);
+                server.Listen(1);
+
+                Task<Socket> serverTask = AcceptAsync(server);
+
+                using (var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    await ConnectAsync(client, server.LocalEndPoint);
+                    Socket accepted = await serverTask;
+
+                    Task sendOne = SendAsync(client, new ArraySegment<byte>(new byte[1]));
+                    Assert.Equal(1, await ReceiveAsync(accepted, new ArraySegment<byte>(new byte[1])));
+                    await sendOne;
+
+                    Console.WriteLine("SendAfterShutdown: About to Shutdown");
+                    client.Shutdown(SocketShutdown.Send);
+                    Console.WriteLine("SendAfterShutdown: Shutdown completed");
+
+                    Task t = SendAsync(client, new ArraySegment<byte>(new byte[1]));
+                    await ShowResult("SendAfterClose", t);
                 }
             }
         }
