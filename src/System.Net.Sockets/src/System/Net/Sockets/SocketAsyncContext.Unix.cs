5cb85b30957f4e07237ef468443e784ce6ee4a85
Geoff Kizer <geoffrek>
2017-08-04 10:54:28 -0700

    fix bug

--- src/System.Net.Sockets/src/System/Net/Sockets/SocketAsyncContext.Unix.cs
+++ src/System.Net.Sockets/src/System/Net/Sockets/SocketAsyncContext.Unix.cs
@@ -654,7 +654,7 @@ namespace System.Net.Sockets
                                 return;
                             }
 
-                            Debug.Assert(_state == QueueState.Waiting, $"_state={_state} while processing queue!");
+                            Debug.Assert(_state == QueueState.Processing, $"_state={_state} while processing queue!");
 
                             if (op == _tail)
                             {
